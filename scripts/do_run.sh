7812902784f5622407c87771846f049557e67472
rozjin <105861976+rozjin@users.noreply.github.com>
2022-12-20 12:49:20 +1300

    Update do_run.sh

--- scripts/do_run.sh
+++ scripts/do_run.sh
@@ -6,7 +6,7 @@ GRADLE_SERVER_DIR=${GRADLE_BUILD_DIR}/server
 GRADLE_FORGE_VER="1.12.2-14.23.5.2860"
 GRADLE_RUN="forge-${GRADLE_FORGE_VER}.jar"
 
-JAVA_RUN="/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64/bin/java"
+JAVA_RUN="/usr/lib/jvm/java-8-openjdk-amd64/bin/java"
 JAVA_CMD="${JAVA_RUN} -jar"
 do_run() {
     cd ${GRADLE_SERVER_DIR}
